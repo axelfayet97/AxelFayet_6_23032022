d77cdac7bf962cb3383ed3b23b7fe9171a854e63
axelfayet97 <axel.gayet97@gmail.com>
2022-03-24 14:07:41 +0100

    Improved delete route, started working on like route

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/Sauces')
+const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
     // req.body.thing objet js sous forme de string
@@ -43,17 +44,15 @@ exports.getOneSauce = (req, res, next) => {
 
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
-        .then((sauce) => {
-            if (!sauce) {
-                return res.status(404).json({ error: new Error('Sauce non trouvée !') });
-            }
-            if (sauce.userId !== req.auth.userId) {
-                return res.status(401).json({ error: new Error('Requête non autorisée !') })
-            }
-            Sauce.deleteOne({ _id: req.params.id })
-                .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
-                .catch(error => res.status(400).json({ error }));
+        .then(sauce => {
+            const filename = sauce.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+                Sauce.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
+                    .catch(error => res.status(400).json({ error }));
+            });
         })
+        .catch(error => res.status(500).json({ error }));
 };
 
-exports.likeSauce
+exports.likeSauce = (req, res, next) => { };
