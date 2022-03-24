9567ea02bf23cd7ad8b5163e57a7a824a89c19e0
axelfayet97 <axel.gayet97@gmail.com>
2022-03-24 13:10:44 +0100

    Routed multer for post and put routes in order to get images

--- routes/product.js
+++ routes/product.js
@@ -1,27 +1,22 @@
 const express = require('express');
 const router = express.Router();
-
-const sauceCtrl = require('../controllers/products');
+const sauceCtrl = require('../controllers/product');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 const { route } = require('./user');
 
 // Routes gestion de données ici
-// Créer une sauce
-router.post('/', auth, sauceCtrl.createSauce);
+// Créer une sauce + image avec Multer
+router.post('/', auth, multer, sauceCtrl.createSauce);
 // Modifier une sauce
-router.put('/:id', auth, sauceCtrl.modifySauce); // Non renseigné ?
+router.put('/:id', auth, multer, sauceCtrl.modifySauce); // Non renseigné ?
 // Récupère l'array de sauce
 router.get('/', auth, sauceCtrl.getAllSauces);
 // Récupère une seule sauce
 router.get('/:id', auth, sauceCtrl.getOneSauce);
-// Capture et enregistrement de l'image
-router.post('/', auth, multer, sauceCtrl.createImageSauce); // à check
-// Assigne l'image à la sauce
-router.put('/:id', auth, multer, sauceCtrl.getImageSauce); // à faire
 // Supprime une sauce
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 // Gestion de like/dislike
-router.post('/:id/like', auth, sauceCtrl.sauceIdLiked);
+// router.post('/:id/like', auth, sauceCtrl.likeSauce);
 
-module.exports = router
+module.exports = router;
