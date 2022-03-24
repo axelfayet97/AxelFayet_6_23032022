2f0af0867e98ec94d9c7c3fd1f98c2df4c6234fe
axelfayet97 <axel.gayet97@gmail.com>
2022-03-24 13:13:29 +0100

    Modified controllers in order to work with multer

--- controllers/product.js
+++ controllers/product.js
@@ -1,35 +1,45 @@
 const Sauce = require('../models/Sauces')
 
 exports.createSauce = (req, res, next) => {
-    delete req.body._id;
+    // req.body.thing objet js sous forme de string
+    const sauceObject = JSON.parse(req.body.sauce);
+
+    delete sauceObject._id;
     // Nouvel objet Sauce
     const sauce = new Sauce({
         // Opérateur spread donnant tous les champs du body
-        ...req.body
+        ...sauceObject,
+        // Génération du nom du fichier de manière dynamique
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     // Methode save dans la bdd
     sauce.save()
-    .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
-    .catch(error => res.status(400).json({ error }));
+        .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
+        .catch(error => res.status(400).json({ error }));
 };
 
 exports.modifySauce = (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    const sauceObject = req.file ?
+        {
+            ...JSON.parse(req.body.sauce),
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        } : { ...req.body };
+    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
         .then(() => res.status(200).json({ message: "OK" }))
-        .catch(error => res.status(400).json({ error }))
+        .catch(error => res.status(400).json({ error }));
 };
 
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
+        .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({ error }));
-    };
-    
-    exports.getOneSauce = (req, res, next) => {
-        Sauce.findOne({ _id: req.params.id })
-            .then(sauce => res.status(200).json(sauce))
-            .catch(error => res.status(404).json({ error }));
-    };
+};
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(404).json({ error }));
+};
 
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
@@ -46,3 +56,4 @@ exports.deleteSauce = (req, res, next) => {
         })
 };
 
+exports.likeSauce
