83935bb62655cf88aba25c7656b614e76c4087a7
axelfayet97 <axel.gayet97@gmail.com>
2022-03-25 18:41:37 +0100

    Control cors with npm cors, fixed route

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // Import d'express, mongoose & body parser
 const { json } = require('express');
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
-const productsRoutes = require('./routes/product');
+const productsRoutes = require('./routes/sauce');
 const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 const path = require('path');
 
 // Connexion à Mongo DB
@@ -22,22 +22,13 @@ mongoose.connect(login, {
 app.use(express.json());
 
 // CORS
-app.use((req, res, next) => {
-    // Autorise les accès à l'API depuis * n'importe quelle origine
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Ajout des headers mentionnés aux requêtes evoyées vers notre API
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    // Permet d'envoyer des requêtes avec les méthodes mentionnées
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    // Exécution de la prochaine étape
-    next();
-});
+app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/auth', userRoutes);
-app.use('api/sauces', productsRoutes);
+app.use('/api/sauces', productsRoutes);
 
 // Export de l'application express
 module.exports = app;
