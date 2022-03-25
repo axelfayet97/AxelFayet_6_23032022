c14ab475b5253693f715a760ccfffba4a18fd5bf
axelfayet97 <axel.gayet97@gmail.com>
2022-03-25 18:40:37 +0100

    Reinforced auth security

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const secretToken = process.env.ACCESS_TOKEN_SECRET;
 
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const decodedToken = jwt.verify(token, secretToken);
         const userId = decodedToken.userId;
         req.auth = { userId };
         if (req.body.userId && req.body.userId !== userId) {
