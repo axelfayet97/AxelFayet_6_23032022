1d5c98e5311110b665f1027e0b6b4eaa6c8ffe9f
axelfayet97 <axel.gayet97@gmail.com>
2022-03-25 18:41:11 +0100

    Started working on like controller

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,7 +1,7 @@
 const Sauce = require('../models/Sauces')
 const fs = require('fs');
 
-exports.createSauce = (req, res, next) => {
+exports.createSauce = (req, res) => {
     const sauceObject = JSON.parse(req.body.sauce);
     // Nouvel objet Sauce
     const sauce = new Sauce({
@@ -10,41 +10,44 @@ exports.createSauce = (req, res, next) => {
         // Génération du nom du fichier de manière dynamique
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
-    console.log(sauce);
     // Methode save dans la bdd
     sauce.save()
         .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
         .catch(error => res.status(400).json({ error }));
 };
 
-exports.modifySauce = (req, res, next) => {
+exports.modifySauce = (req, res) => {
     const sauceObject = req.file ?
         {
             ...JSON.parse(req.body.sauce),
             imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
         } : { ...req.body };
+    // Supprimer current image
     Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
         .then(() => res.status(200).json({ message: "OK" }))
         .catch(error => res.status(400).json({ error }));
+
 };
 
-exports.getAllSauces = (req, res, next) => {
-    console.log('CONTROLLER SAUCE');
+exports.getAllSauces = (req, res) => {
     Sauce.find()
         .then(sauces => res.status(200).send(sauces))
         .catch(error => res.status(400).json({ error }));
 };
 
-exports.getOneSauce = (req, res, next) => {
+exports.getOneSauce = (req, res) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => res.status(200).json(sauce))
         .catch(error => res.status(404).json({ error }));
 };
 
-exports.deleteSauce = (req, res, next) => {
+exports.deleteSauce = (req, res) => {
+    // Trouve l'id de la sauce à delete
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => {
+            // On retire /images/ du nom du chemin
             const filename = sauce.imageUrl.split('/images/')[1];
+            console.log("sauce : " + sauce, "filename : " + filename);
             fs.unlink(`images/${filename}`, () => {
                 Sauce.deleteOne({ _id: req.params.id })
                     .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
@@ -54,6 +57,33 @@ exports.deleteSauce = (req, res, next) => {
         .catch(error => res.status(500).json({ error }));
 };
 
-// exports.likeSauce = (req, res, next) => {
-//     Sauce.findOne({})
-// };
+exports.likeSauce = (req, res) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(likeControl => {
+            const thumbController = req.body.like;
+            // Si like
+            if (thumbController == 1) {
+                if (likeControl.usersLiked == req.body.userId) {
+                    // Si user id déjà sauvé
+
+                } else {
+                    likeControl.usersLiked.push(req.body.userId);
+                }
+                likeControl.likes++;
+                // likeControl.usersLiked = [];
+            } else if (thumbController == -1) {
+                // Si dislike
+                // likeControl.usersDisliked ?  : likeControl.usersDisliked.push(req.body.userId);
+                likeControl.likes--;
+                // likeControl.usersDisliked = [];
+            } else {
+                // Si défaut : on retire dans tous les cas userId des 2 array
+                console.log(likeControl.usersLiked);
+                likeControl.usersLiked.splice(indexOf(req.body.userId), 1);
+                likeControl.usersDisliked.splice(indexOf(req.body.userId), 1);
+            }
+            likeControl.save();
+            res.status(200).json({ message: 'Sauce likée !', userLiked: req.body.userId })
+        })
+        .catch(error => (res.status(500).json({ error })));
+};
