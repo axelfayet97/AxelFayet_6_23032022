105203b370bb2668477805791cd6a287cd5d6d76
axelfayet97 <axel.gayet97@gmail.com>
2022-04-08 19:30:31 +0200

    Improved comments

--- middleware/validator.js
+++ middleware/validator.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validate = require('mongoose-validator');
 
+// Validation pour les champs input Name, Manufacturer
 exports.inputValidator = [
     validate({
         validator: 'isLength',
@@ -13,6 +14,8 @@ exports.inputValidator = [
         message: "Vous ne pouvez utiliser que des chiffres et des lettres pour nommer votre sauce",
     })
 ];
+
+// Validation pour le champs input Main Pepper
 
 exports.pepperValidator = [
     validate({
